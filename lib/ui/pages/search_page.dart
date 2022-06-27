889b0e47db452b2f69861e633189a0fcaa365753
Evgeny Alekseenko <punconge@gmail.com>
2022-06-27 15:27:38 +1000

    search page

--- lib/ui/pages/search_page.dart
+++ lib/ui/pages/search_page.dart
@@ -1,10 +1,121 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SearchPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart';
+import 'package:rick_and_morty_serach/bloc/character_bloc.dart';
+import 'package:rick_and_morty_serach/data/models/character.dart';
+
+class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
 
   @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  late Character _currentCharacter;
+  List<Results> _currentResults = [];
+  int _currentPage = 1;
+  String _currentSearchStr = '';
+
+  @override
+  void initState() {
+    if (_currentResults.isEmpty) {
+      context
+          .read<CharacterBloc>()
+          .add(const CharacterEvent.fetch(name: '', page: 1));
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    final state = context.watch<CharacterBloc>().state;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding:
+              const EdgeInsets.only(top: 15, bottom: 15, left: 16, right: 16),
+          child: TextField(
+            style: const TextStyle(color: Colors.white),
+            cursorColor: Colors.white,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: const Color.fromRGBO(86, 86, 86, 0.8),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide.none,
+              ),
+              prefixIcon: const Icon(Icons.search, color: Colors.white),
+              hintText: 'Search Name',
+              hintStyle: const TextStyle(color: Colors.white),
+            ),
+            onChanged: (value) {
+              _currentPage = 1;
+              _currentResults = [];
+              _currentSearchStr = value;
+              context
+                  .read<CharacterBloc>()
+                  .add(CharacterEvent.fetch(name: value, page: _currentPage));
+            },
+          ),
+        ),
+        Expanded(
+          child: state.when(
+            loading: () {
+              return Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    CircularProgressIndicator(
+                      strokeWidth: 2,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text('Loading...'),
+                  ],
+                ),
+              );
+            },
+            loaded: (characterLoaded) {
+              _currentCharacter = characterLoaded;
+              _currentResults = _currentCharacter.results;
+              return _currentResults.isNotEmpty
+                  ? _customListView(_currentResults)
+                  : const SizedBox();
+            },
+            error: () => const Text('Nothing found...'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _customListView(List<Results> currentResults) {
+    return ListView.separated(
+      itemCount: currentResults.length,
+      separatorBuilder: (_, index) => const SizedBox(
+        height: 5.0,
+      ),
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        final result = currentResults[index];
+        return Padding(
+          padding: const EdgeInsets.only(
+              right: 16.0, left: 16.0, top: 3.0, bottom: 3.0),
+          child: ListTile(
+            title: Text(
+              result.name,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
